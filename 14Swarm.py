189487d4413c834b7f0198b9eb845e7d5910bbf3
aaresposit <aaron.hoddy@gmail.com>
2026-05-01 01:55:05 -0400

    Update and rename 14Swarm 1.py to 14Swarm.py

--- 14Swarm.py
+++ 14Swarm.py
@@ -1,7 +1,12 @@
+# updated 4/29/2026
+# intended to be the main file
+
 import drone
 from codrone_edu.swarm import *
-swarm = Swarm(enable_color=False, enable_print=False)
+
+swarm = Swarm(enable_color=True, enable_print=True, enable_pause=False)
 cycle_num = 0
+
 
 def color_cycle():
     # Code that alternates between red and blue everytime it is called
@@ -10,12 +15,12 @@ def color_cycle():
     global g
     global b
     global cycle_num
-    if cycle_num%2:
-        r,g,b = 255,0,0 # red
+    if cycle_num % 2:
+        r, g, b = 255, 0, 0  # red
     else:
-        r,g,b = 0,0,255 # blue
+        r, g, b = 0, 0, 255  # blue
     cycle_num += 1
-    
+
 
 def setup_drones():
     # Counts the number of drones and assigns colors
@@ -26,9 +31,9 @@ def setup_drones():
             swarm.run_drone(drone_count, "set_drone_LED", r, g, b, brightness=255)
             drone_count += 1
         except:
-            print(f"There are {drone_count} drones connected")
             return drone_count
             break
+
 
 def move_drones(*drone_count):
     # tries to move the drones. prints error if fails.
@@ -40,13 +45,25 @@ def move_drones(*drone_count):
     except Exception as e:
         print(f"Could not move. Error: {e}")
 
+
 def main():
     swarm.connect()
-    swarm.takeoff()
-    swarm.hover()
     drone_count = setup_drones()
-    print(drone_count)
-    swarm.land()
+    print(f"{drone_count} drones detected.")
+    while True:
+        swarm.run_drone(0, "set_drone_LED", r=255, g=0, b=0, brightness=255)
+        swarm.run_drone(1, "set_drone_LED", r=0, g=0, b=255, brightness=255)
+        time.sleep(1)
+        swarm.run_drone(0, "set_drone_LED", r=0, g=0, b=255, brightness=255)
+        print(swarm.run_drone(1, "get_battery"))
+        time.sleep(1)
+        swarm.run_drone(0, "send_absolute_position", 0.5, 0, 0.5, 0.5, 0, 0)
+        swarm.run_drone(1, "set_drone_LED", r=255, g=0, b=0, brightness=255)
+        time.sleep(1)
+    """swarm.takeoff()
+    swarm.hover()
+    move_drones()
+    swarm.land()"""
     swarm.disconnect()
 
 
