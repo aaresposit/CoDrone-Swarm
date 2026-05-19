6defb8af45d7c7c1c59ad4827f255de0b926c114
aaresposit <aaron.hoddy@gmail.com>
2026-05-19 15:38:09 -0400

    Update and rename Sequence Test.py to SequenceExample.py

--- SequenceExample.py
+++ SequenceExample.py
@@ -1,33 +1,33 @@
-# updated 5/1/2026
-# test code to be used with three drones, demonstrating independent flight / actions
-# uncomment movement once verified this works
+# 5/19/2026
+# example code to be used with three drones, demonstrating independent flight / actions in a sequence
+# comment/uncomment movement to verify this works
 
 from codrone_edu.swarm import *
 
 seq_0 = Sequence(0)
-# seq_0.add('takeoff')
-# seq_0.add('send_absolute_position', 0.5, 0, 0, 0.5, 0, 0)
-# seq_0.add('hover', 0.5)
+seq_0.add('takeoff')
+seq_0.add('send_absolute_position', 0.5, 0, 0, 0.5, 0, 0)
+seq_0.add('hover', 0.5)
 seq_0.add('set_drone_LED', 255, 25, 255, 255) # purple
 
 seq_1 = Sequence(1)
-# seq_1.add('takeoff')
-# seq_1.add('set_yaw', 50)
-# seq_1.add('move', 1)
-# seq_1.add('hover', 0.5)
+seq_1.add('takeoff')
+seq_1.add('set_yaw', 50)
+seq_1.add('move', 1)
+seq_1.add('hover', 0.5)
 seq_1.add('set_drone_LED', 25, 255, 255, 255) # cyan
 
 seq_2 = Sequence(0)
-# seq_2.add('send_absolute_position', -0.5, 0, 0, 0.5, 0, 0)
-# seq_2.add('hover', 0.5)
-# seq_2.add('land')
+seq_2.add('send_absolute_position', -0.5, 0, 0, 0.5, 0, 0)
+seq_2.add('hover', 0.5)
+seq_2.add('land')
 seq_2.add('set_drone_LED', 25, 255, 255, 255) # cyan
 
 seq_3 = Sequence(1)
-# seq_3.add('set_yaw', -50)
-# seq_3.add('move', 1)
-# seq_3.add('hover', 0.5)
-# seq_3.add('land')
+seq_3.add('set_yaw', -50)
+seq_3.add('move', 1)
+seq_3.add('hover', 0.5)
+seq_3.add('land')
 seq_3.add('set_drone_LED', 255, 25, 255, 255) # purple
 
 seq_4 = Sequence(3)
