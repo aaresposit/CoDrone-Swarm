465a7aada9e9a7429376cd5d82b3748ef3027ad0
aaresposit <aaron.hoddy@gmail.com>
2026-05-19 11:27:09 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,30 @@
-# 5/18/26
+# 5/19/26
 # client file that receives an instruction to run choreography file
 
 import socket
+import drone_choreography   # import name of file that contains choreography code
 
-HOST = "192.168.0.196" # server computer's ip
-PORT = 9999 # server port
+# Connect to the socket that the server is using
+HOST = "10.197.228.127" # server computer's ip
+PORT = 9999 # server computer's port
 s = socket.socket()
-
 s.connect((HOST,PORT))
-print("Connected")
+print("Connected.")
 
+# Loop to constantly listen for messages
+# if the message is "run", run file we imported;
+# if the message is "exit", break out of loop
+# else if the message is something else, print the message
 while True:
     data = s.recv(1024)
-    command = data.decode("utf-8")
-    if command == "go":
-        reply = "received"
-        s.send(reply.encode("utf-8"))
+    message = data.decode("utf-8")
+    if message == "run":
         print("Running file.")
-        import test_flight   # insert name of file to run here
-        s.close()
+        drone_choreography.main()
+    elif message == "exit":
+        break
+    else:
+        print(message)
+# When loop stops, close connection
+s.close()
+print("Connection closed.")
