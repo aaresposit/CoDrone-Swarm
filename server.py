24bf11a2774f1ea4de4399da39079febfc16e7ef
aaresposit <aaron.hoddy@gmail.com>
2026-05-19 12:08:25 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,48 +1,53 @@
 # 5/18/2026
 # Server file that sends command to all clients to run their choreography files and runs its own.
 
+# Step 1: Set up socket
 import socket
 HOST = ""
 PORT = 9999
 s = socket.socket()
+s.bind((HOST,PORT))
+s.listen()
 
 
-# Step 1: Bind
-def bind_socket():
-    try:
-        s.bind((HOST,PORT))
-        s.listen(5)
-    except:
-        print("Retry bind\r")
-        bind_socket()
-bind_socket()
-
-
-# Step 2: Accept (3 connections)
+# Step 2: Accept Connections
 print("How many computers will connect to this one?")
-amount_connecting = None
-while type(amount_connecting) != int:
+# Loop and get user input
+# If whole number greater than 0, break out of loop
+while True:
     try:
-        amount_connecting = int(input())
+        amount_connecting = int(input("Enter a whole number greater than zero:"))
+        if amount_connecting > 0:
+            break
     except:
-        print("Please enter a whole number.")
-
+        pass
+# Begin to connect to computers
+# Create lists to store info
 print(f"Okay, waiting for {amount_connecting} computers to connect.")
 connections = []
 addresses = []
+# For each of the # of computers: connect to a computer, store its information, and tell it which number it is
 for computer in range(amount_connecting):
     connection,address = s.accept()
     connections.append(connection)
     addresses.append(address)
-    print(f"Connection established: {address}")
+    print(f"Connection {computer+1} established: {address}")
+    message = f"This computer is connection {computer+1}."
+    connection.send(message.encode("utf-8"))
 
 
 # Step 3: Send data
-command = input("Type 'go' to start:")
-for conn in connections:
-    conn.send(str.encode(command))
-    conn.close()
-import test_flight   # insert name of file to be executed
+while True:
+    message = input("Type 'run' to run choreography files, or 'exit' to disconnect all computers: ")
+    message = message.lower()
+    for conn,addr in zip(connections,addresses):
+        try:
+            conn.send(message.encode("utf-8"))
+            if message == "exit":
+                conn.close()
+        except:
+            print(f"Could not send to {addr}. Check client computer.")
+    if message == "run":
+        drone_choreography.main()   # same name as import with .main() attached to end
 s.close()
-print("connection was closed")
-
+print("Connection was closed.")
